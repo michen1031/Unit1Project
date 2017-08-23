91f1f551f473574b0f2508a0208c13122a80f7d4
Chen <michelle.chen@capitalone.com>
2017-08-23 16:22:11 -0400

    added css

--- main.js
+++ main.js
@@ -10,263 +10,257 @@ $(function() {
   startGame();
 
   //USER PICKS A PLAYER
-  $('#catdog').on('click', function() {
-    character = 1;
-    $('#ostrichBut').css("background", "white");
-    $('#catdog').css("background", "yellow");
-    $('#pikaBut').css("background", "white");
-    $('#homerBut').css("background", "white");
-    displayAnimal(players[character]);
-  });
+    $('#catBut').on('click', function() {
+      character = 1;
+      $('#ostrichBut').css("background", "#9acd32");
+      $('#catBut').css("background", "#FD5B5B");
+      $('#pikaBut').css("background", "#9acd32");
+      $('#homerBut').css("background", "#9acd32");
+      displayAnimal(players[character]);
+    });
 
-  $('#ostrichBut').on('click', function() {
-    character = 2;
-    $('#catdog').css("background", "white");
-    $('#ostrichBut').css("background", "yellow");
-    $('#pikaBut').css("background", "white");
-    $('#homerBut').css("background", "white");
-    displayAnimal(players[character]);
-  });
+    $('#ostrichBut').on('click', function() {
+      character = 2;
+      $('#catBut').css("background", "#9acd32");
+      $('#ostrichBut').css("background", "#FD5B5B");
+      $('#pikaBut').css("background", "#9acd32");
+      $('#homerBut').css("background", "#9acd32");
+      displayAnimal(players[character]);
+    });
 
-  $('#pikaBut').on('click', function() {
-    character = 3;
-    $('#ostrichBut').css("background", "white");
-    $('#catdog').css("background", "white");
-    $('#pikaBut').css("background", "yellow");
-    $('#homerBut').css("background", "white");
-    displayAnimal(players[character]);
-  });
+    $('#pikaBut').on('click', function() {
+      character = 3;
+      $('#ostrichBut').css("background", "#9acd32");
+      $('#catBut').css("background", "#9acd32");
+      $('#pikaBut').css("background", "#FD5B5B");
+      $('#homerBut').css("background", "#9acd32");
+      displayAnimal(players[character]);
+    });
 
-  $('#homerBut').on('click', function() {
-    character = 4;
-    $('#ostrichBut').css("background", "white");
-    $('#catdog').css("background", "white");
-    $('#pikaBut').css("background", "white");
-    $('#homerBut').css("background", "yellow");
-    displayAnimal(players[character]);
-  });
+    $('#homerBut').on('click', function() {
+      character = 4;
+      $('#ostrichBut').css("background", "#9acd32");
+      $('#catBut').css("background", "#9acd32");
+      $('#pikaBut').css("background", "#9acd32");
+      $('#homerBut').css("background", "#FD5B5B");
+      displayAnimal(players[character]);
+    });
 
-  function displayAnimal(animal) {
-    $('.bottom').empty();
-    $('.bottom').append(`<img id = ${players[character]} src = images/${animal}.gif><img id = "apple2" src = "images/apple.png">`)
-    console.log(players[character]);
-  }
+    function displayAnimal(animal) {
+      $('.bottom').empty();
+      $('.bottom').append(`<img id = ${players[character]} src = images/${animal}.gif><img id = "apple2" src = "images/apple.png">`)
+      console.log(players[character]);
+    }
 
 
   // GET FIND INITIAL POSITION OF ANIMALS AND RESET THEM WHEN REPLAYING THE GAME
-  var horse_postion = getInitPositionOfAnimal('horse');
-  var player_postion = getInitPositionOfAnimal(players[character]);
+    var horse_postion = getInitPositionOfAnimal('horse');
+    var player_postion = getInitPositionOfAnimal(players[character]);
 
-  function getInitPositionOfAnimal(animal) {
-    return $('#' + animal).position();
-  }
+    function getInitPositionOfAnimal(animal) {
+      return $('#' + animal).position();
+    }
 
-  // function isPlayer(animal) {
-  //   return (animal === players[character]) ? true : false
-  // }
+    function isCat(animal) {
+      return (animal === "cat") ? true : false
+    }
 
-  function isCat(animal) {
-    return (animal === "cat") ? true : false
-  }
-
-  function resetAnimalPositions(animals) {
-    animals.map((animal) => {
-      let set_translate = (isCat(animal))
-        ? `translate(0rem) scaleX(-1)`
-        : `translate(0rem)`
-      $('#' + animal).css({'transform': set_translate});
-    })
-  }
+    function resetAnimalPositions(animals) {
+      animals.map((animal) => {
+        let set_translate = (isCat(animal))
+          ? `translate(0rem) scaleX(-1)`
+          : `translate(0rem)`
+        $('#' + animal).css({'transform': set_translate});
+      })
+    }
 
 
   //EASY MEDIUM HARD BUTTONS
-  var easyButton = $('#easy').on('click', function() {
-    level = 0;
-    $('#easy').css("background", "yellow");
-    $('#med').css("background", "white");
-    $('#hard').css("background", "white");
-    return level;
-  });
-
-  var medButton = $('#med').on('click', function() {
-    level = 1;
-    $('#med').css("background", "yellow");
-    $('#easy').css("background", "white");
-    $('#hard').css("background", "white");
-    return level;
-  });
-
-  var hardButton = $('#hard').on('click', function() {
-    level = 2;
-    $('#hard').css("background", "yellow");
-    $('#easy').css("background", "white");
-    $('#med').css("background", "white");
-    return level;
-  });
-
-
-  //start button //run
-  function startGame(levelNum) {
-    resetAnimalPositions([players[character], 'horse']);
-    $('#start').on('click', function() {
-      if(level == -1) {
-        alert("Please select a difficulty!");
-        return;
-      }
-      if(character == 0) {
-        alert("Please select a player!");
-        return;
-      }
-      goHorse = setInterval(running, 400);
-      $('#run').on('click', function() {
-        if(level == 0) {
-          position2 += 8;
-        }
-        else if(level == 1) {
-          position2 += 6;
-        }
-        else if(level == 2) {
-          position2 += 4;
-        }
-        console.log("player is", players[character]);
-        console.log("position2 is", position2);
-        if(players[character] == "cat") {
-          $(`#${players[character]}`).css({'transform': `translate(${position2}rem) scaleX(-1)`});
-        }
-        else if(players[character] != "cat") {
-          $(`#${players[character]}`).css({'transform': `translate(${position2}rem)`});
-        }
-
-        if(collision($(`#${players[character]}`), $('#apple2'))) {
-          clearInterval(Interval);
-          clearInterval(goHorse);
-          var imageName = `${players[character]}apple`;
-          var imageType = "jpg";
-          var raceWinner = "You"
-          theWinner(imageName, imageType, raceWinner);
-        }
-      });
+    var easyButton = $('#easy').on('click', function() {
+      level = 0;
+      $('#easy').css("background", "#FD5B5B");
+      $('#med').css("background", "#9acd32");
+      $('#hard').css("background", "#9acd32");
+      return level;
     });
-  }
 
-  //pause button
-  $('#pause').on('click', function() {
-    clearInterval(Interval);
-    clearInterval(goHorse);
-  });
+    var medButton = $('#med').on('click', function() {
+      level = 1;
+      $('#med').css("background", "#FD5B5B");
+      $('#easy').css("background", "#9acd32");
+      $('#hard').css("background", "#9acd32");
+      return level;
+    });
+
+    var hardButton = $('#hard').on('click', function() {
+      level = 2;
+      $('#hard').css("background", "#FD5B5B");
+      $('#easy').css("background", "#9acd32");
+      $('#med').css("background", "#9acd32");
+      return level;
+    });
 
 
-//horse running
-  var running = function () {
-    position1 += 10;
-    $('#horse').css({'transform': `translate(${position1}rem)`});
-    if(collision($('#horse'), $('#apple1'))) {
-      clearInterval(goHorse);
-      clearInterval(Interval);
-      var imageName = "horseapple";
-      var imageType = "jpeg";
-      var raceWinner = "You lose! The horse";
-      theWinner(imageName, imageType, raceWinner);
+  //START GAME
+    function startGame() {
+      resetAnimalPositions([players[character], 'horse']);
+      $('#start').on('click', function() {
+        if(level == -1) {
+          alert("Please select a difficulty!");
+          return;
+        }
+        if(character == 0) {
+          alert("Please select a player!");
+          return;
+        }
+
+        goHorse = setInterval(running, 400);
+        $('#run').css("background", "#FD5B5B").css("font-size", "2em").css("padding", ".7em");
+        $('#run').on('click', function() {
+          if(level == 0) {
+            position2 += 7;
+          }
+          else if(level == 1) {
+            position2 += 5;
+          }
+          else if(level == 2) {
+            position2 += 3;
+          }
+          if(players[character] == "cat") {
+            $(`#${players[character]}`).css({'transform': `translate(${position2}rem) scaleX(-1)`});
+          }
+          else if(players[character] != "cat") {
+            $(`#${players[character]}`).css({'transform': `translate(${position2}rem)`});
+          }
+          if(collision($(`#${players[character]}`), $('#apple2'))) {
+            clearInterval(Interval);
+            clearInterval(goHorse);
+            var imageName = `${players[character]}apple`;
+            var imageType = "jpg";
+            var raceWinner = "You"
+            theWinner(imageName, imageType, raceWinner);
+          }
+        });
+      });
     }
-  }
+
+  //PAUSE BUTTON
+    $('#pause').on('click', function() {
+      clearInterval(Interval);
+      clearInterval(goHorse);
+    });
+
+
+  //horse running
+    var running = function () {
+      position1 += 10;
+      $('#horse').css({'transform': `translate(${position1}rem)`});
+      if(collision($('#horse'), $('#apple1'))) {
+        clearInterval(goHorse);
+        clearInterval(Interval);
+        var imageName = "horseapple";
+        var imageType = "jpeg";
+        var raceWinner = "You lose! The horse";
+        theWinner(imageName, imageType, raceWinner);
+      }
+    }
 
 
   //ANNOUNCE THE WINNER
-  function theWinner(animalpic, imageType, winner) {
-    //display playAgain button
-    $('#playAgain').css("display", "block");
-    $('.buttonContainer').css("display", "none");
-    //HIDE THE RACETRACK
-    $('.top').hide();
-    $('.bottom').hide();
-    $('#start').off('click');
-    $('#run').off('click');
+    function theWinner(animalpic, imageType, winner) {
+      //display playAgain button
+        $('#playAgain').css("display", "block");
+        $('.buttonContainer').css("display", "none");
+      //HIDE THE RACETRACK
+        $('.top').hide();
+        $('.bottom').hide();
+        $('#start').off('click');
+        $('#run').off('click');
 
-    //DISPLAY WINNER TEXT/IMAGES
-    $('.racetrack').append(`<div class = "winnerDiv"><img class = "winner" src = images/${animalpic}.${imageType}></div>`).css("border", "white").append(`<p class = "winText"> ${winner} got the apple first! </p>`);
-  }
+      //DISPLAY WINNER TEXT/IMAGES
+        $('.racetrack').append(`<div class = "winnerDiv"><img class = "winner" src = images/${animalpic}.${imageType}></div>`).css("border", "#9acd32").append(`<p class = "winText"> ${winner} got the apple first! </p>`);
+      }
 
   //PLAY THE GAME AGAIN
-  $('#playAgain').on('click', function() {
-    //EMPTY WINNER DIVS
-    $('.winnerDiv').remove();
-    $('.winText').remove();
-    //DISPLAY RACETRACK/BUTTONS
-    $('.racetrack').children().css("display", "block");
-    $('.racetrack').css("border", "black solid 1px");
-    $('.buttonContainer').css("display", "block");
-    $('#playAgain').css("display", "none");
-    $('#easy').css("background", "white");
-    $('#med').css("background", "white");
-    $('#hard').css("background", "white");
-    startGame();
-    $('.top').show()
-    $('.bottom').show()
-    //START THE TIMER OVER
-    clearInterval(Interval);
-    tens = "00";
-    seconds = "00";
-    appendTens.innerHTML = tens;
-    appendSeconds.innerHTML = seconds;
-    //REPOSITION CHARACTERS
-    position1 = 0;
-    position2 = 0;
-    level = -1;
-    });
-
-
-/*
-TIMER TIMER TIMER TIMER
-  */
-  var seconds = 00;
-  var tens = 00;
-  var appendTens = document.getElementById("tens")
-  var appendSeconds = document.getElementById("seconds")
-  var buttonStart = document.getElementById('start');
-  var Interval;
-
-  buttonStart.onclick = function() {
-    if(character != 0 && level != -1) {
+    $('#playAgain').on('click', function() {
+      //EMPTY WINNER DIVS
+      $('.winnerDiv').remove();
+      $('.winText').remove();
+      //DISPLAY RACETRACK/BUTTONS
+      $('.racetrack').children().css("display", "block");
+      $('.racetrack').css("border", "black solid 1px");
+      $('.buttonContainer').css("display", "block");
+      $('#playAgain').css("display", "none");
+      $('#easy').css("background", "#9acd32");
+      $('#med').css("background", "#9acd32");
+      $('#hard').css("background", "#9acd32");
+      startGame();
+      $('.top').show();
+      $('.bottom').show();
+      $('#run').css("background","#9acd32").css("font-size", "1em").css("padding", ".3em");
+      //START THE TIMER OVER
       clearInterval(Interval);
-      Interval = setInterval(startTimer, 10);
-    }
-  }
+      tens = "00";
+      seconds = "00";
+      appendTens.innerHTML = tens;
+      appendSeconds.innerHTML = seconds;
+      //REPOSITION CHARACTERS
+      position1 = 0;
+      position2 = 0;
+      level = -1;
+      });
 
-  function startTimer () {
-    tens++;
-    if(tens < 9){appendTens.innerHTML = "0" + tens;}
-    if (tens > 9){appendTens.innerHTML = tens;}
-    if (tens > 99) {
-      seconds++;
-      appendSeconds.innerHTML = "0" + seconds;
-      tens = 0;
-      appendTens.innerHTML = "0" + 0;
+
+  //TIMER
+    var seconds = 00;
+    var tens = 00;
+    var appendTens = document.getElementById("tens")
+    var appendSeconds = document.getElementById("seconds")
+    var buttonStart = document.getElementById('start');
+    var Interval;
+
+    buttonStart.onclick = function() {
+      if(character != 0 && level != -1) {
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
+      }
     }
-    if (seconds > 9){appendSeconds.innerHTML = seconds;}
-  }
+
+    function startTimer () {
+      tens++;
+      if(tens < 9){appendTens.innerHTML = "0" + tens;}
+      if (tens > 9){appendTens.innerHTML = tens;}
+      if (tens > 99) {
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+      }
+      if (seconds > 9){appendSeconds.innerHTML = seconds;}
+    }
 
   //colliding
-  function collision(img1, img2) {
-    if (img1 && img2) {
-      var x1 = img1.offset().left;
-      var y1 = img1.offset().top;
-      var h1 = img1.outerHeight(true);
-      var w1 = img1.outerWidth(true);
-      var b1 = y1 + h1;
-      var r1 = x1 + w1;
-      var x2 = img2.offset().left;
-      var y2 = img2.offset().top;
-      var h2 = img2.outerHeight(true);
-      var w2 = img2.outerWidth(true);
-      var b2 = y2 + h2;
-      var r2 = x2 + w2;
-      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
-        return false;
+    function collision(img1, img2) {
+      if (img1 && img2) {
+        var x1 = img1.offset().left;
+        var y1 = img1.offset().top;
+        var h1 = img1.outerHeight(true);
+        var w1 = img1.outerWidth(true);
+        var b1 = y1 + h1;
+        var r1 = x1 + w1;
+        var x2 = img2.offset().left;
+        var y2 = img2.offset().top;
+        var h2 = img2.outerHeight(true);
+        var w2 = img2.outerWidth(true);
+        var b2 = y2 + h2;
+        var r2 = x2 + w2;
+        if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+          return false;
+        }
+        return true;
       }
-      return true;
-    } else {
-      console.log(img1, img2, 'why you no work');
+      else {
+        console.log(img1, img2, 'why you no work');
+      }
     }
-  }
-
 });
